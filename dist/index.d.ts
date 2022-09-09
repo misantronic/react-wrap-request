d444f4052721dd7907409a1621fc83c6f7f0a5c9
david.schkalee <david.schkalee@sportalliance.com>
2022-09-09 20:17:03 +0200

    chore: compile code

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
 declare type ToupleArray = ReadonlyArray<any> | readonly [any];
-interface WrapRequestOptions<Y, T> {
+export interface ReactWrapRequestOptions<Y, T> {
     deps?: Y;
     defaultData?: T;
     cacheKey?: string;
 }
-export declare function useWrapRequest<T, Y extends ToupleArray>(req: (...deps: Y) => Promise<T>, options?: WrapRequestOptions<Y, T>): import("wrap-request").WrapRequest<T, Y[0], T, any>;
+export declare function useWrapRequest<T, Y extends ToupleArray>(req: (...deps: Y) => Promise<T>, options?: ReactWrapRequestOptions<Y, T>): import("wrap-request").WrapRequest<T, Y[0], T, any>;
 export {};
